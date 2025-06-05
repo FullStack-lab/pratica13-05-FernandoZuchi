8a926a0aa01c5e13e1be28b44ccfaa2063965c06
FernandoZuchi <nandozuchi07@gmail.com>
2025-06-05 12:00:47 -0300

    ✨feat + 🧹 cleanup: limpeza geral do projero react e criação do 1º componente

---
+++ fundamentos-react/src/components/MyComponent.jsx
@@ -0,0 +1,10 @@
+function MyComponent() {
+  return (
+    <div>
+      <h1>Meu componente</h1>
+      <p>Este é meu primeiro componente react.</p>
+    </div>
+  );
+}
+
+export default MyComponent;
